6c2e7afaf0fcace25b49484da424a422dd0b36c1
aizazhussain46 <aizaz.hussain@orangeroomdigital.com>
2021-03-31 20:05:19 +0500

    fields columns modify

---
+++ database/migrations/2021_03_31_133247_add_nominee_to_fields_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddNomineeToFieldsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('fields', function (Blueprint $table) {
+            $table->longtext('nominee_details')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('fields', function (Blueprint $table) {
+            $table->dropColumn('nominee_details');
+        });
+    }
+}
